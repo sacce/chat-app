9d734c19dbad02718bc10d2dc9a5156541f1bae1
Teemu Kotiharju <teemu.kotiharju@gmail.com>
2017-10-13 10:57:26 +0300

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+const socket = io();
+
+socket.on('connect', function() {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'Teemu',
+    text: 'Jees, toimii...'
+  });
+});
+
+socket.on('disconnect', function() {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+  console.log('newMessage', message);
+});
--- server/server.js
+++ server/server.js
@@ -13,6 +13,16 @@ app.use(express.static(publicPath));
 
 io.on('connection', socket => {
   console.log('New user connected');
+
+  socket.emit('newMessage', {
+    from: 'Frank',
+    text: 'Whatever, see you then!'
+  });
+
+  socket.on('createMessage', message => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
